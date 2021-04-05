d74d0328330dbcd5a23ee39fd5be8ac7d291a3ca
Suveer Prithipal <suveerp@orionhealth.com>
2021-04-06 07:12:51 +1200

    added code comments

--- integration/laybuy/search.js
+++ integration/laybuy/search.js
@@ -1,12 +1,16 @@
+
+// method to search for a store
 function WhenISearchForAMerchant(shop){
     cy.get('input[type="search"]').scrollIntoView().type(shop,{force: true});
     cy.get('input[type="search"]').should('have.value', shop);
 }
 
+// method to validate the shop directory
 function ThenIExpectAShopDirectoryCountOf(expectedLength){
     cy.get('[class^=shop-directory-]').not('[class*=shop-directory-module--tag-matches]').should('have.length', expectedLength);
 }
 
+// setup method for each test
 context('Store Search Smoke Test', () => {
     beforeEach(() => {
         cy.visit('https://sandbox-www.laybuy.com/nz/shop-here/');
@@ -14,12 +18,16 @@ context('Store Search Smoke Test', () => {
         cy.wait(1000);
     });
 
+// Test that Stirling produces 2 entries in the shop directory
     describe('Stirling should return 2 results', () => {
         it('I search for Stirling and expect 2 results', (shop = 'Stirling', expectedLength = 2) => {
             WhenISearchForAMerchant(shop);
             ThenIExpectAShopDirectoryCountOf(expectedLength);
         })
     });
+
+// Side Quest: Ensure that we can reuse the test method and that it works for other shops
+    // PBTech should return 1 store
     describe('SideQuest: PBTech should return 1 result', () => {
         it('I search for PbTech and expect 1 results', (shop = 'PbTech', expectedLength = 1) => {
             WhenISearchForAMerchant(shop);
@@ -27,6 +35,8 @@ context('Store Search Smoke Test', () => {
         })
     });
 
+// Test the opening of the store in another window
+    // Test for attributes of the href
     describe('Open Store in new Window', () => {
         it('I click Stirling and expect the store to open in a new window', (shop = 'Stirling') => {
             WhenISearchForAMerchant(shop);
