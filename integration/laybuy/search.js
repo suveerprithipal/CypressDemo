8cfaba70e975420441a0b30b4dfc493f1fd42ee2
Suveer Prithipal <suveerp@orionhealth.com>
2021-04-06 07:29:09 +1200

    added code comments

--- integration/laybuy/search.js
+++ integration/laybuy/search.js
@@ -1,4 +1,5 @@
 
+// Please use firefox as chrome 
 // method to search for a store
 function WhenISearchForAMerchant(shop){
     cy.get('input[type="search"]').scrollIntoView().type(shop,{force: true});
@@ -6,8 +7,8 @@ function WhenISearchForAMerchant(shop){
 }
 
 // method to validate the shop directory count
-function ThenIExpectAShopDirectoryCountOf(expectedLength){
-    cy.get('[class^=shop-directory-]').not('[class*=shop-directory-module--tag-matches]').should('have.length', expectedLength);
+function ThenIExpectAShopDirectoryCountOf(expectedCount){
+    cy.get('[class^=shop-directory-]').not('[class*=shop-directory-module--tag-matches]').should('have.length', expectedCount);
 }
 
 // setup method for each test
@@ -20,18 +21,18 @@ context('Store Search Smoke Test', () => {
 
 // Test that Stirling produces 2 entries in the shop directory
     describe('Stirling should return 2 results', () => {
-        it('I search for Stirling and expect 2 results', (shop = 'Stirling', expectedLength = 2) => {
+        it('I search for Stirling and expect 2 results', (shop = 'Stirling', expectedCount = 2) => {
             WhenISearchForAMerchant(shop);
-            ThenIExpectAShopDirectoryCountOf(expectedLength);
+            ThenIExpectAShopDirectoryCountOf(expectedCount);
         })
     });
 
 // Side Quest: Ensure that we can reuse the test method and that it works for other shops
     // PBTech should return 1 store
     describe('SideQuest: PBTech should return 1 result', () => {
-        it('I search for PbTech and expect 1 results', (shop = 'PbTech', expectedLength = 1) => {
+        it('I search for PbTech and expect 1 results', (shop = 'PbTech', expectedCount = 1) => {
             WhenISearchForAMerchant(shop);
-            ThenIExpectAShopDirectoryCountOf(expectedLength);
+            ThenIExpectAShopDirectoryCountOf(expectedCount);
         })
     });
 
@@ -44,7 +45,6 @@ context('Store Search Smoke Test', () => {
             cy.get('[class^=shop-directory-module--tiles]').not('[class*=shop-directory-module--tag-matches]')
                 .within(($form) => {
                     cy.get('[class*=tile-module--tile]').eq(0).should('have.attr', 'target', '_blank').click();
-                    cy.go('back');
             })
         })
     });
